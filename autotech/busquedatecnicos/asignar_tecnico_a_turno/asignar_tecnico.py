3e778b445891eeb9ea35f42d3431243719ca64af
LuciaSoria <luciacsoria5@gmail.com>
2023-05-10 14:47:22 -0300

    Corrigiendo datos de asignar_tecnico.py

--- autotech/busquedatecnicos/asignar_tecnico_a_turno/asignar_tecnico.py
+++ autotech/busquedatecnicos/asignar_tecnico_a_turno/asignar_tecnico.py
@@ -1,4 +1,4 @@
-from datetime import date, time
+from datetime import date, time, datetime
 from administracion.models import Turno_taller
 
 def asignar_tecnico(id_tecnico:int, id_turno: int):
@@ -6,14 +6,14 @@ def asignar_tecnico(id_tecnico:int, id_turno: int):
     turno = obtener_turno(id_turno)
     
     if not turno_valido(turno):
-        print("error")
+        raise ValueError("El turno no paso la prueba de papeles aun")
     if not coinciden_los_talleres(tecnico, turno):
-        print("error")
-    if not esta_disponible():
-        print("error")
+        raise ValueError("El tecnico no trabaja en el taller de ese turno")
+    if not esta_disponible(id_tecnico):
+        raise ValueError("El tecnico no tiene disponible ese horario")
     
     turno.tecnico_id = id_tecnico   # agregamos el id del tecnico al turno
-    asignar_turno(tecnico, turno.id_turno) # esto es un POST a la api de tecnicos
+    asignar_turno(tecnico, turno.id_turno) # esto es un POST a la api de tecnicos, creo
     turno.estado = "en_proceso"
     
 def obtener_turno(id_turno) -> Turno_taller:
@@ -28,15 +28,21 @@ def turno_valido(turno: Turno_taller):
         es_valido = False
     return es_valido
   
+ #TODO 
 def coinciden_los_talleres(tecnico, turno:Turno_taller):
     print("TODO")
     
-def esta_disponible(tecnico, dia:date, hora_inicio:time, hora_fin:time):
+def esta_disponible(id_tecnico: int, dia:date, hora_inicio:time, hora_fin:time):
     turnos_del_tecnico = obtener_turnos_del_tecnico(id_tecnico)
     esta_disponible = True
+    
     for turno in turnos_del_tecnico:
-        if turno.fecha_inicio == dia:   # Transformar la fecha_inicio a date
-            esta_disponible = esta_disponible and not hay_superposicion(turno.hora_inicio, turno.hora_fin, hora_inicio, hora_fin):
+        dia_inicio_turno = turno.fecha_inicio
+        if dia_inicio_turno == dia:
+            hora_inicio_turno = turno.hora_inicio
+            hora_fin_turno = turno.hora_fin
+            esta_disponible = esta_disponible and not hay_superposicion(hora_inicio_turno, hora_fin_turno, hora_inicio, hora_fin)
+    
     return esta_disponible
             
 def hay_superposicion(hora_inicio1: time, hora_fin1: time, hora_inicio2: time, hora_fin2: time):
@@ -46,9 +52,11 @@ def hay_superposicion(hora_inicio1: time, hora_fin1: time, hora_inicio2: time, h
     caso4 = hora_fin1 <= hora_inicio2 # el 1 termina cuando el 2 empieza
     return caso1 or caso2 or caso3 or caso4
     
+#TODO    
 def obtener_tecnico(id_tecnico: int):
     print("TODO")
     
+#TODO    
 def asignar_turno(tecnico, id_turno: int):
     print("TODO")
 
