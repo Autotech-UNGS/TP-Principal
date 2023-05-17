9bfae78f0b7233b7ece32279743ca1123ea67919
Joacosan <jacoosanchezz@gmail.com>
2023-05-17 05:18:47 -0300

    arreglo url

--- autotech/autotech/urls.py
+++ autotech/autotech/urls.py
@@ -4,7 +4,7 @@ from django.urls import path, include
 
 urlpatterns = [
     #path('', include('administracion.urls')),
-    path('', admin.site.urls),
+    path('admin/', admin.site.urls),
     path('turnos/', include('turnos.urls')),
     path('evaluaciones/', include('evaluaciones.urls')),
     path('tecnicos/', include('tecnicos.urls'))
