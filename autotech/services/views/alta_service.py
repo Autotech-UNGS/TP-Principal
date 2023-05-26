9b628305f44f0f5fb413791f78073b2371a65cf4
Joacosan <jacoosanchezz@gmail.com>
2023-05-26 09:11:54 -0300

    create service completo con validaciones

--- autotech/services/views/alta_service.py
+++ autotech/services/views/alta_service.py
@@ -1,5 +1,8 @@
 import json
 
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
 from rest_framework import permissions
 from rest_framework.response import Response
 from rest_framework import status
@@ -15,57 +18,50 @@ from services.validadores import *
 
 class ServiceCreate(APIView):
     permission_classes = [permissions.AllowAny]
-    
-    # marca, modelo, frecuencia_km, costo_base, id_supervisor, id_tasks
+
     # { "marca": "Renault", "modelo": "206", "frecuencia_km":"15000", "costo_base":"10000.0","id_supervisor":"1","id_tasks":"[1,2,3,4,5]"}
     def post(self, request, *args, **kwargs):
         validador = ValidadorService()
+
         try:
             validador.validar_service(request)
         except ValidationError as e:
             error_messages = [str(error) for error in e.detail]
             return Response({'error': error_messages}, status=status.HTTP_400_BAD_REQUEST)
         
-        
-        marca = request.data.get('marca')
-        modelo = request.data.get('modelo')
+        marca = (request.data.get('marca')).lower()
+        modelo = (request.data.get('modelo')).lower()
         frecuencia_km = request.data.get('frecuencia_km')
         costo_base = request.data.get('costo_base')
         id_supervisor = request.data.get('id_supervisor')
-
-        # capturo las id_tasks que son del service creado y calculo su duracion y costo total
         id_tasks = request.data.get('id_tasks')
         id_task_lista = json.loads(id_tasks)
+
+        try:
+            validador.validar_tareas(id_task_lista)
+        except ValidationError as e:
+            error_messages = [str(error) for error in e.detail]
+            return Response({'error': error_messages}, status=status.HTTP_400_BAD_REQUEST)
         #print(id_task_lista)
 
         costo_total, duracion_total = calcular_costo_y_duracion_total(id_task_lista)
         # print(costo_total, duracion_total)
         costo_total += costo_base
 
-        marca = marca.lower()
-        modelo = modelo.lower()
-
-
         service = Service.objects.create(marca=marca, modelo=modelo, frecuencia_km = frecuencia_km, costo_base = costo_base
                                          , costo_total = costo_total, duracion_total = duracion_total
                                          , id_supervisor = id_supervisor)
-        serializer = ServiceSerializer(service)
-
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-    
-
-
-    """ if not Turno_taller.objects.filter(id_turno=id_turno).exists():
-            return Response({'error': 'El turno pasado no existe'}, status=status.HTTP_400_BAD_REQUEST)
         
-        turno = Turno_taller.objects.get(id_turno = id_turno)
-        if not turno.tipo == "evaluacion":
-            return Response({'error': 'El turno pasado no es un turno para Evaluación'}, status=status.HTTP_400_BAD_REQUEST)
-        
-        if Registro_evaluacion.objects.filter(id_turno=id_turno).exists():
-            return Response({'error': 'El turno pasado ya existe en los registros'}, status=status.HTTP_400_BAD_REQUEST)
-        # Tomo el turno que corresponde a ese id
-        turno_taller = Turno_taller.objects.get(pk=id_turno) """
+        service_tasks = Service_tasks.objects.create(id_service = service, id_tasks = id_tasks)
+
+        serializer_tasks = ServiceTasksSerializer(service_tasks)
+        serializer_service = ServiceSerializer(service)
+
+        response_data = {
+            "service":serializer_service.data,
+            "service_tasks":serializer_tasks.data
+        }
+        return Response(response_data, status=status.HTTP_201_CREATED)
     
 
 def calcular_costo_y_duracion_total(lista):
