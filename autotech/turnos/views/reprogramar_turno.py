5a16fdb05132f53f3bc8e01058f4900349af3fac
LuciaSoria <luciacsoria5@gmail.com>
2023-05-28 15:57:38 -0300

    corrigiendo reprogramar turno

--- autotech/turnos/views/reprogramar_turno.py
+++ autotech/turnos/views/reprogramar_turno.py
@@ -24,11 +24,12 @@ class ReprogramarTurnoViewSet(ViewSet):
         turno = self.obtener_turno(id_turno)
         if turno == None:
             return HttpResponse("error: no hay un turno con la id solicitada.", status=400)
-        taller_id = turno.taller_id.id_taller
+        taller_id = str(turno.taller_id.id_taller)
         vieja_fecha_inicio = turno.fecha_inicio
         vieja_hora_inicio = turno.hora_inicio
         vieja_fecha_fin = turno.fecha_fin
         vieja_hora_fin = turno.hora_fin
+        print(taller_id)
         
         duracion = obtener_duracion(vieja_fecha_inicio, vieja_hora_inicio, vieja_fecha_fin, vieja_hora_fin)
         nueva_fecha_hora_fin = obtener_fecha_hora_fin(nueva_fecha_inicio, nueva_hora_inicio, duracion)
@@ -49,12 +50,27 @@ class ReprogramarTurnoViewSet(ViewSet):
         frecuencia_km = turno.frecuencia_km
         papeles_en_regla = turno.papeles_en_regla
         
-        nuevo_turno = Turno_taller.objects.create(tipo= tipo, estado= estado, taller_id = taller_id, patente=patente, 
+        nuevo_turno = Turno_taller.objects.create(tipo= tipo, estado= estado, taller_id = turno.taller_id, patente=patente, 
                                                   fecha_inicio=nueva_fecha_inicio, hora_inicio= nueva_hora_inicio, 
                                                   fecha_fin= nueva_fecha_hora_fin[0], hora_fin=nueva_fecha_hora_fin[1], 
                                                   frecuencia_km= frecuencia_km, papeles_en_regla= papeles_en_regla)
-        nuevo_turno_json = serializers.serialize('json', [nuevo_turno])
-        return JsonResponse(nuevo_turno_json, safe=False)
+        nuevo_turno_data = serializers.serialize('json', [nuevo_turno])
+        nuevo_turno_data = json.loads(nuevo_turno_data)[0]['fields']
+        nuevo_turno_data['id_turno'] = nuevo_turno.id_turno
+        nuevo_turno_data['tipo'] = nuevo_turno.tipo
+        nuevo_turno_data['estado'] = nuevo_turno.estado
+        nuevo_turno_data['taller_id'] = nuevo_turno.taller_id.id_taller
+        nuevo_turno_data['tecnico_id'] = nuevo_turno.tecnico_id
+        nuevo_turno_data['patente'] = nuevo_turno.patente
+        nuevo_turno_data['fecha_inicio'] = nuevo_turno.fecha_inicio
+        nuevo_turno_data['hora_inicio'] = nuevo_turno.hora_inicio
+        nuevo_turno_data['fecha_fin'] = nuevo_turno.fecha_fin
+        nuevo_turno_data['hora_fin'] = nuevo_turno.hora_fin
+        nuevo_turno_data['frecuencia_km'] = nuevo_turno.frecuencia_km
+        nuevo_turno_data['papeles_en_regla'] = nuevo_turno.papeles_en_regla
+        
+        return JsonResponse(nuevo_turno_data, json_dumps_params={'indent': 4})
+        #return JsonResponse(nuevo_turno_json, safe=False)
         
 # ---------------------------------------------------------------------------------------- #
 # ------------------------------------- validaciones ------------------------------------- #
