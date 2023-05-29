a6ead3e99ec693b4822e7d95aa56e20837673e03
Joacosan <jacoosanchezz@gmail.com>
2023-05-29 19:27:36 -0300

    arreglo evaluaciones

--- autotech/talleres/urls.py
+++ autotech/talleres/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('crear/', alta_talleres.TalleresCreate.as_view(), name = 'talleres-crear'),
     path('modificar/<int:id_taller>/', modificar_talleres.ModificarTaller.as_view(), name = 'talleres-modificar'),
     path('sucursales-validas-activas/', visualizar_sucursales.VisualizarSucursalesConTallerValidas.as_view(), name = 'sucursales-list'),
-    path('sucursales-validas-activas/<int:id_sucursal>', visualizar_sucursales.VisualizarUnaSucursalConTallerValida.as_view(), name = 'sucursales-list-una'),
+    path('sucursales-validas-activas/<int:id_sucursal>/', visualizar_sucursales.VisualizarUnaSucursalConTallerValida.as_view(), name = 'sucursales-list-una'),
     path('sucursales-sin-taller/', visualizar_sucursales.VisualizarSucursalesSinTaller.as_view(), name = 'sucursales-list-sin-taller'),
 
 
